664cdfdca3dc8747db8290c9d6bd933a254eebd0
Siew Boon Keat <sbkeat0316@gmail.com>
2026-04-16 17:16:44 +0800

    stepbystep

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-testing123
+step by step
+
+# 1. command prompt
+# 2. open folder inside command prompt
+# 3. git pull origin *branch* 
+# 4. check branch, git branch
+# 5. switch to which branch you want 
+# 6. make changes in file
+# 7. after change do git add .
+# 8. git commit -m "implement __, added __, remove __, fixed __."
+# 9. git push -u origin *branch*
