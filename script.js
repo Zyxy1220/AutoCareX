474ca43dddd184541d1c0c217fb38b052b731677
Zyxy1220 <chewzhenyang1112@gmail.com>
2026-04-19 00:34:24 +0800

    Testing java to create animation for switching picture

---
+++ script.js
@@ -0,0 +1,30 @@
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+const nextBtn = document.getElementById('nextBtn');
+
+let currentIndex = 0;
+let timer = null;
+
+function gotoslide(index) {
+    slides[currentIndex].classList.remove('active');
+    dots[currentIndex].classList.remove('active');
+
+    currentIndex = (index + slides.length) % slides.length;
+
+    slides[currentIndex].classList.add('active');
+    dots[currentIndex].classList.add('active');
+}
+
+function startAutoPlay() {
+    timer = setInterval(() => {
+        gotoslide(currentIndex + 1);
+    }, 5000);  // 5s to change picture and the index will += 1
+}
+
+nextBtn.addEventListener('click',() => {
+    clearInterval(timer);
+    gotoslide(currentIndex + 1);
+    startAutoPlay();
+})
+
+startAutoPlay();
