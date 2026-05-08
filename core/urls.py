1f2b0ae4409dc965bdecfa0e9a5dbe789e401729
Salah-Yusuf <salahudin.yusuf312@gmail.com>
2026-05-08 09:26:12 +0800

    verification test settled

--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('login/')),
     path('admin/', admin.site.urls),
     path('login/', include('login.urls')),
     path('ev/', include('ev_calculation.urls')),
